3f6c63d13f1c9a6c95724f1249ab19ee771969dd
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-03 16:19:03 +0100

    [setup]-configure-application-assets-and-constants-in-pubspecyaml-file

---
+++ lib/supporting_files/constants.dart
@@ -0,0 +1,12 @@
+//Images
+String triangleImg = "bomb.png";
+String bgImg = "background.png";
+String kioubImg(int index) => "monster-$index.png";
+String expImg(int index) => "e-$index.png";
+
+//Audio
+String bgMusic = "music.mp3";
+String expMusic = "explosion.mp3";
+
+//Font
+String fontName = "Thirteen-Pixel-Fonts";
